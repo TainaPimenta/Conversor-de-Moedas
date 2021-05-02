5e7da3984099e609be26b6d5ac40ffb643c312ab
TainaPimenta95 <81496791+TainaPimenta95@users.noreply.github.com>
2021-05-02 20:46:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const botao =
+      document.querySelector('button')
+
+botao.onclick = () =>  {
+  
+ var valorEmDolar =
+Number(document.querySelector('input').
+          value.replace(',',','))
+
+document.querySelector('h2').innerHTML = 'R$'+
+ (valorEmDolar*5.52).toFixed(2)
+
+  
+}
